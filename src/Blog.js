7c2bb46efa3186c6d5b71204d2172d96f31b5218
ashonweb <meghpalbhagya@gmail.com>
2019-03-21 05:53:41 +0530

    added blog page

--- src/Blog.js
+++ src/Blog.js
@@ -3,8 +3,27 @@ import React , {Component} from 'react'
  export default class About extends Component{
   render(){
     return (
-      <div>
-       <h1>hikadjkajdkdkajdkj</h1>
+      <div className="blogcontainer">
+       <div className="javascriptblog">
+          
+       </div>
+       <div className="react" >
+        
+       </div>
+       <div className="html ">
+
+                  <div class="middletext">
+                    <div className="textblog">
+                    
+                    <a href = "https://google.com"> Blog on HTML</a>
+                    </div>
+               
+                
+                
+                
+                </div>
+              
+       </div>
       </div>
       
     )
